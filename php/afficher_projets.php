90b73dc22f1452b8d4084482ba95d125d1a65be6
khalilRhouma <khalil.rhoumaghardedou@ensi-uma.tn>
2018-10-11 22:46:28 +0100

    Styling the HTML pages: login,register and insertion_projet

--- php/afficher_projets.php
+++ php/afficher_projets.php
@@ -16,7 +16,7 @@ session_start();
         }
         </style>
 
-  <title>Bootstrap Card</title>
+  <title>Afficher les projets</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
